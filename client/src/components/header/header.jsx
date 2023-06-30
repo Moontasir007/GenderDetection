a1a82140fadc61abaab1c89f8b51a3f1f6a7486b
Moontasir Mamun <moontasir.csecu@gmail.com>
2023-06-30 21:26:24 +0600

    gsap animation added in header component

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,12 +1,38 @@
-import "./header.css"
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import "./header.css";
 
-export default function header() {
+export default function Header() {
+    const titleRef = useRef(null);
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const tl = gsap.timeline({ delay: 0.5 });
+
+        tl.fromTo(
+            titleRef.current,
+            { opacity: 0, y: -20 },
+            { opacity: 1, y: 0, duration: 1 }
+        ).fromTo(
+            imageRef.current,
+            { opacity: 0, y: 20 },
+            { opacity: 1, y: 0, duration: 1 },
+            "-=0.5"
+        );
+    }, []);
+
     return (
         <div className="header">
-            <div className="headerTitles">
-                <span className="headerTitlesSm">React & Node</span><span className="headerTitlesLg">Blog</span>
+            <div className="headerTitles" ref={titleRef}>
+                <span className="headerTitlesSm">A SIMPLE HELP TOOL</span>
+                <span className="headerTitlesLg">GENDER</span>
             </div>
-            <img src="https://img.freepik.com/free-photo/group-elephants-big-green-tree-wilderness_181624-16897.jpg?size=626&ext=jpg&uid=R64718410&ga=GA1.2.299819484.1668252753&semt=sph" alt="" className="headerImg" />
+            <img
+                src="https://img.freepik.com/free-photo/group-elephants-big-green-tree-wilderness_181624-16897.jpg?size=626&ext=jpg&uid=R64718410&ga=GA1.2.299819484.1668252753&semt=sph"
+                alt=""
+                className="headerImg"
+                ref={imageRef}
+            />
         </div>
-    )
+    );
 }

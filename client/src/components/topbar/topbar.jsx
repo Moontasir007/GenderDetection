b0decf368976945d01026acf9098a6d6cfccb3d0
Moontasir Mamun <moontasir.csecu@gmail.com>
2023-06-30 20:47:51 +0600

    gsap added in topbar

--- client/src/components/topbar/topbar.jsx
+++ client/src/components/topbar/topbar.jsx
@@ -1,16 +1,27 @@
-import { useContext } from "react";
+import { gsap } from "gsap";
+import { useContext, useEffect, useRef } from "react";
 import { Context } from "../../context/Context";
-import "./topbar.css"
-import { Link } from "react-router-dom"
+import "./topbar.css";
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
     const { user, dispatch } = useContext(Context);
+    const topRef = useRef(null);
+
+    useEffect(() => {
+        gsap.fromTo(
+            topRef.current,
+            { opacity: 0, y: -20 },
+            { opacity: 1, y: 0, duration: 1 }
+        );
+    }, []);
 
     const handleLogout = () => {
         dispatch({ type: "LOGOUT" });
-    }
+    };
+
     return (
-        <div className="top">
+        <div className="top" ref={topRef}>
             <div className="topLeft">
                 <i className="topIcon fa-brands fa-square-facebook"></i>
                 <i className="topIcon fa-brands fa-square-twitter"></i>
@@ -20,19 +31,29 @@ export default function Topbar() {
             <div className="topCenter">
                 <ul className="topList">
                     <li className="topListItem">
-                        <Link className="link" to="/">HOME</Link>
+                        <Link className="link" to="/">
+                            HOME
+                        </Link>
                     </li>
                     <li className="topListItem">
-                        <Link className="link" to="/classify">CLASSIFY</Link>
+                        <Link className="link" to="/classify">
+                            CLASSIFY
+                        </Link>
                     </li>
                     <li className="topListItem">
-                        <Link className="link" to="/contact">CONTACT</Link>
+                        <Link className="link" to="/contact">
+                            CONTACT
+                        </Link>
                     </li>
                     <li className="topListItem">
-                        <Link className="link" to="/write">WRITE</Link>
+                        <Link className="link" to="/write">
+                            WRITE
+                        </Link>
                     </li>
                     <li className="topListItem">
-                        <Link className="link" to="/tutorial">TUTORIAL</Link>
+                        <Link className="link" to="/tutorial">
+                            TUTORIAL
+                        </Link>
                     </li>
                     <li className="topListItem" onClick={handleLogout}>
                         {user && "LOGOUT"}
@@ -40,22 +61,24 @@ export default function Topbar() {
                 </ul>
             </div>
             <div className="topRight">
-                {
-                    user ? (
-                        <img src={user.profilePic} alt="" className="topImg" />
-
-                    ) : (
-                        <ul className="topList">
-                            <li className="topListItem">
-                                <Link className="link" to="/login">LOGIN</Link></li>
-                            <li className="topListItem">
-                                <Link className="link" to="/register">REGISTER</Link>
-                            </li>
-                        </ul>
-                    )
-                }
+                {user ? (
+                    <img src={user.profilePic} alt="" className="topImg" />
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">
+                                LOGIN
+                            </Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">
+                                REGISTER
+                            </Link>
+                        </li>
+                    </ul>
+                )}
                 <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
-    )
+    );
 }
